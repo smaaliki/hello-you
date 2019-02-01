ce7c76e07771f9bc22454a997cb6843ca7a7c395
Samer Maaliki <smaaliki@hotmail.com>
2019-02-01 19:13:21 +0400

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Samer Maaliki</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href='/css/helloyou.css' type='text/css' rel='stylesheet'>
+
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Samer Maaliki
+        </div>
+        <div>
+            <iframe width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/fxlF7IHa62Q"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+        </div>
+        <div>
+            <p>
+                Hello everyone!! This is my submission for <span style='font-style:italic'>"Assignment #0 - Hello, you!"</span> for the <span style='font-style:italic; font-weight:900'>Introduction to creative Exploration on the Web</span> course
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
